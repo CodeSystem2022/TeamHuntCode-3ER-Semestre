cade9e534d9aa2d30018043629c51684d1d46512
Bergerat Santiago <70027319+Yago030@users.noreply.github.com>
2023-04-18 07:52:15 -0300

    clase dos python, completada

--- Python/Leccion2/archivo_con_with.py
+++ Python/Leccion2/archivo_con_with.py
@@ -1,10 +1,13 @@
-try:
-    #manejo de contexto with, sintaxis simplificada
-    with open('prueba.txt', 'r', encoding='utf8') as archivo:
-        print(archivo.read())
+from Leccion2.ManejoArchivos import ManejoArchivos  #manejo de contexto with, sintaxis simplificada
+    # with open('prueba.txt', 'r', encoding='utf8') as archivo:
+    #    print(archivo.read())
 
 
-except Exception as e:
-    print(e)
-finally:
-    print("finalizo correctamente")
+
+#no nos hace falta el bloque try ni finally
+ # Utiliza diferentes metodos : __enter__ este es el que abre
+# ahora el siguiente metodo  :  __exit__ este lo cierra
+
+with ManejoArchivos('prueba.txt') as archivo:
+    print(archivo.read())
+
