b103d6eab87d8d79c52838f96d8e5945f017c583
santiago bergerat <santiagobergerat@gmail.com>
2023-05-11 23:44:38 -0300

    actualizando leccion 04 de js

--- Javascript/TSLeccion04/01-04-Objetos.js
+++ Javascript/TSLeccion04/01-04-Objetos.js
@@ -15,6 +15,10 @@ let persona ={
 
     nombreCompleto : function (){ //creacion de metodos en js
         return this.nombre + ' '  + this.apellido;
+    },
+
+    get nombreEdad() { 
+        return ' El nombre es: ' + this.nombre + ' edad  ' + this.edad;
     }
 }
 
@@ -84,6 +88,128 @@ console.log(personaArray);
 //numero 4 utilizaremos el metodo JSON.stringify
 let personaString = JSON.stringify(persona2);
 
-console.log(JSON.stringify(persona)); //aca usando el metodo
+console.log(JSON.stringify(persona)); //aca usando el metodo 
 
 console.log(personaString);
+
+
+let persona3 ={
+    nombre: ' Ramoncito ',
+    edad : 39,
+    email: ' ramoncito@gmail.com ',
+    idioma: ' es ',
+
+    pesonaCompleta : function(){
+    return this.nombre + ' , su mail es:  ' + this.email+ ' Su edad: ' + this.edad+ ' maneja el idioma: '+ this.idioma;
+    },
+    get lang(){
+        return this.idioma.toUpperCase();
+    },
+
+
+    set lang(lang){
+         this.idioma = lang.toUpperCase();
+    },
+
+   
+    get nombreEdad3(){
+
+    },
+
+
+
+
+}
+
+
+console.log(persona.nombreEdad);
+
+
+console.log('Comenzamos con los metodos:   get para identificar idiomas  ')
+console.log(persona3.lang);
+
+
+function persona3_1(nombre, apellido, email){
+    this.nombre = nombre;
+    this.email = email;
+    this.apellido = apellido;
+
+    this.nombreCompleto = function (){
+        return this.nombre + '  '+this.apellido+' '+this.email;
+    }
+
+}
+
+
+let padre = new persona3_1 (' Leo ', ' Lopez ', 'Leolopez@gmail.com ');
+padre.nombre ='Luis';
+console.log(padre)
+
+console.log(padre.nombreCompleto())
+
+
+let madre = new persona3_1(' ines ', ' Diaz ', ' Id@gmail.com ')
+console.log(madre.nombreCompleto())
+
+
+console.log(madre)
+
+let miObjeto = new Object(); // esta es la opcion formal 
+
+let miObjeto2 ={}; //opcion recomendada
+
+let miCadena1 = new String (' Hola '); // sintaxis formal
+
+let miCadena2 = 'Hola'; //sintaxis recomendada y simplicada 
+
+let miNumero = new Number(1); //sintaxis formal
+
+let miNumero2 = 1; //sintaxis recomendada
+
+
+let booleano1 = new Boolean(false)// sintaxis formal
+
+let boolean2= true; //sintaxis recomendada
+
+let miArreglo1 = new Array(); //formal
+
+let miArreglo2=[];//recomendado
+
+let miFucion1 = new Function (); // todo before new is object
+
+let miFuncion2 = function(){}; //recomended
+
+
+
+
+//uso de PROTOTYPE 
+
+persona3_1.prototype.telefono = '2613482839';
+console.log(padre)
+
+
+
+//uso de CALL
+
+let persona4 = {
+    nombre: ' Juan ',
+    apellido: ' Perez ',
+    nombreCompleto3 : function() {
+       // return titulo+ ' ' + this.nombre+ '  '+ this.apellido+ ' '+ telefono ;
+       return this.nombre+ ' '+ this.apellido;
+    }
+}
+
+
+let persona5 ={
+    nombre:' Rosalia ',
+    apellido: ' Monzon',
+
+}
+
+console.log(persona4.nombreCompleto3('Lic. ', '23232'));
+console.log(persona4.nombreCompleto3.call(persona5,'Ing. ', '23224334'));
+
+
+//Metodo Apply
+
