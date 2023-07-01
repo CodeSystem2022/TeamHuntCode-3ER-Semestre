0a5b253f9d194d5c0efcb11b98843aa961cb8adb
Mayra Medina <111921764+Maynh@users.noreply.github.com>
2023-07-01 00:06:35 -0300

    Parte 2_calculadora

---
+++ Java/caluladora_parte2.java
@@ -0,0 +1,40 @@
+private static void mostrarMenu() {
+        // Mostrar menu
+        System.out.println("""
+                1. Suma
+                2. Resta
+                3. Multiplicación
+                4. División
+                5. Salir
+                """);
+        System.out.println("¿Operación a realizar?");
+    }
+
+    private static void ejecutarOperacion(int operacion, Scanner entrada) {
+        System.out.print("Digite el valor para el operando1: ");
+        var operando1 = Double.parseDouble(entrada.nextLine());
+        System.out.print("Digite el valor para el operando2: ");
+        var operando2 = Double.parseDouble(entrada.nextLine());
+
+        Double resultado;
+        switch (operacion) {
+            case 1 -> {
+                resultado = operando1 + operando2;
+                System.out.println("Resultado de la suma: " + resultado);
+            }
+            case 2 -> {
+                resultado = operando1 - operando2;
+                System.out.println("Resultado de la resta: " + resultado);
+            }
+            case 3 -> {
+                resultado = operando1 * operando2;
+                System.out.println("Resultado de la multiplicación: " + resultado);
+            }
+            case 4 -> {
+                resultado = operando1 / operando2;
+                System.out.println("Resultado de la división: " + resultado);
+            }
+            default -> System.out.println("Opción errónea: " + operacion);
+        } // fin del switch
+    }
+}
